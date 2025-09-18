522c83f0e350e889b5c333e8e77ed60dd9391a21
Javier Piñeyro <dariojavierpineyro@gmail.com>
2025-09-17 23:23:59 -0300

    Add api router file and routes

---
+++ backend/routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\PacienteController;
+use App\Http\Controllers\PetController;
+use App\Http\Controllers\TratamientoController;
+
+Route::apiResource('pacientes', PacienteController::class)->only(['index','store', 'show', 'update']);
+Route::apiResource('tratamientos', TratamientoController::class)->only(['index','store']);
+
+Route::apiResource('pets', PetController::class)->only(['index', 'store', 'show', 'update']);
+Route::get('pets/activos', [PetController::class, 'listAllActive']);
