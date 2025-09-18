2a88c2af27c04f21ada455b0e0fb38b0b6011bb9
Javier Piñeyro <dariojavierpineyro@gmail.com>
2025-09-18 00:08:12 -0300

    Fix error on importing Models

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $pet = App\Models\Pet::create([
+        $pet = \App\Models\Pet::create([
             'nombre' => 'PET-CT Tomografía',
             'color' => 'verde',
             'activo' => true,
@@ -20,14 +20,14 @@ class DatabaseSeeder extends Seeder
             'intensidad' => 4,
             'observaciones' => 'Ninguna'
         ]);
-        $paciente = App\Models\Paciente::create([
+        $paciente = \App\Models\Paciente::create([
             'nombre' => 'Luis Alfonso',
             'apellido' => 'Gómez',
             'sexo' => 'M',
             'dni' => 32904231,
             'fecha_nacimiento' => '1980-05-15',
         ]);
-        $tratamientos = App\Models\Tratamiento::create([
+        $tratamientos = \App\Models\Tratamiento::create([
             'paciente_id' => $paciente->id,
             'pet_id' => $pet->id,
             'fecha_inicio' => '2025-09-18',
